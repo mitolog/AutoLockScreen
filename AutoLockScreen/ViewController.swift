37fd9e44343421216d7a0d9828fe0586b10eb7d0
mitolog <hkyhm5l@gmail.com>
2015-12-15 01:12:14 +0700

    initial commit

---
+++ AutoLockScreen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLockScreen
+//
+//  Created by Yuhei Miyazato on 12/15/15.
+//  Copyright © 2015 mitolab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
